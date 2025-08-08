0fda91018d7a8c542e5ec62a4ba7bda182d34772
Ricardo Albero Rico <albero992@gmail.com>
2025-08-08 19:14:19 +0200

    Estructura de carpetas.

---
+++ src/main/java/org/example/Cliente.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public class Cliente {
+    private String nombre;
+    private String dni;
+    private String telefono;
+    private String email;
+    private String direccion;
+
+    public Cliente(String nombre, String dni, String telefono, String email, String direccion) {
+        this.nombre = nombre;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+    }
+}
+
